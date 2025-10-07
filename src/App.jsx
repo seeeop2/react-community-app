bc23afe3ea9d09c968ed3d28726b4e326924c04c
seeeop2 <seeeop2@gmail.com>
2025-10-07 22:41:53 +0900

    FEAT: React Router를 이용한 페이지 라우팅 설정
    - Home, Notfound 페이지 컴포넌트 신규 생성
    - react-router-dom의 BrowserRouter, Routes, Route를 사용하여 각 URL 경로에 맞는 페이지가 렌더링되도록 라우팅 구조 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import './App.css'
+import {Route, Routes} from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import Notfound from "./pages/Notfound.jsx";
 
 function App() {
 
   return (
-      <>
-      </>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/*" element={<Notfound/>}/>
+      </Routes>
   )
 }
 
