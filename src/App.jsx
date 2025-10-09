53f74ba94570b3aa2b72b3388cc26ad0c90e8ffd
seeeop2 <seeeop2@gmail.com>
2025-10-09 19:47:14 +0900

    FEAT: Post 목록을 위한 초기 Mock 데이터 및 상태 정의
    - App 컴포넌트 내 useState를 활용한 posts 상태 관리 초기화
    - 게시판 기능을 위한 초기 mockData 설정

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,34 @@ import './App.css'
 import {Route, Routes} from "react-router-dom";
 import Home from "./pages/Home.jsx";
 import Notfound from "./pages/Notfound.jsx";
+import {useState} from "react";
+
+const mockData = [
+  {
+    id: 1,
+    title: "게시글 1",
+    author: "운영자",
+    date: new Date("2026-03-09").getTime(),
+    category: "공지"
+  },
+  {
+    id: 2,
+    title: "게시글 2",
+    author: "사용자2",
+    date: new Date("2026-03-10").getTime(),
+    category: "정보"
+  },
+  {
+    id: 3,
+    title: "게시글 3",
+    author: "사용자3",
+    date: new Date("2026-03-11").getTime(),
+    category: "잡담"
+  },
+]
 
 function App() {
+  const [posts, setPosts] = useState(mockData);
 
   return (
       <Routes>
